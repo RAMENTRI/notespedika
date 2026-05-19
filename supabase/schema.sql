6bcd4edc37f8314e92710aec26eb386a3f165105
Ram-zandora <sreeram.mr@entri.me>
2026-05-19 21:34:05 +0530

    Remove blocking auth signup trigger

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -88,9 +88,6 @@ end;
 $$;
 
 drop trigger if exists on_auth_user_created on auth.users;
-create trigger on_auth_user_created
-  after insert on auth.users
-  for each row execute procedure public.handle_new_user();
 
 create or replace function public.ensure_user_profile()
 returns public.users
